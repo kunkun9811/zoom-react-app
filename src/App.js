016937f5ed5cdac1029750b4574f10fdb72cf95d
kunkun9811 <timochen77@gmail.com>
2021-08-28 14:57:56 -0700

    testing iframe and parent communication

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Zoom from "./components/Zoom";
 
 function App() {
+  /***** KEY: DEBUG: testing iframe and parent communication *****/
+  useEffect(() => {
+    console.log("HELLO FROM iframe");
+    window.top.postMessage("From Iframe!", "*");
+  }, []);
+  /***** END DEBUG: *****/
+
   return <Zoom />;
 }
 

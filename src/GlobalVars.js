01d7f71989e38bbc1b934589b80b04f8643e57c6
kunkun9811 <timochen77@gmail.com>
2021-08-31 13:55:18 -0700

    added process.env.NODE_ENV for some

--- src/GlobalVars.js
+++ src/GlobalVars.js
@@ -1,4 +1,10 @@
-/* data types sending to parent dom container */
+/*** variables for Zoom Config in ZoomGlobalVars.js ***/
+const DEV_LEAVE_URL = "https://localhost:3001";
+const PROD_LEAVE_URL = "https://www.zoomdemo.aankh.co/overlay";
+export const ZOOM_LEAVE_URL = !process.env.NODE_ENV || process.env.NODE_ENV === "development" ? DEV_LEAVE_URL : PROD_LEAVE_URL;
+export const ZOOM_SIGNATURE_ENDPOINT = "https://www.zoomdemo.aankh.co/signature";
+
+/*** data types sending to parent dom container ***/
 export const dataTypeToParentDOM = {
   TYPE_MEETING_NUMBER: 0,
   TYPE_USER_ROLE: 1,
--- src/components/Zoom/zoomGlobalVars.js
+++ src/components/Zoom/zoomGlobalVars.js
@@ -1,12 +1,14 @@
+import { ZOOM_SIGNATURE_ENDPOINT, ZOOM_LEAVE_URL } from "../../GlobalVars";
+
 /* zoom configurations */
 export const zoomConfig = {
   // setup your signature endpoint here: https://github.com/zoom/websdk-sample-signature-node.js
   // TODO: depending on DEVELOPMENT or DEPLOYMENT
-  signatureEndpoint: "https://www.zoomdemo.aankh.co/signature",
+  signatureEndpoint: ZOOM_SIGNATURE_ENDPOINT,
   apiKey: "PqT9I5o7Q--GqvZXDOMl9w",
   role: 0, // TODO: this might be dynamic in the future
   // TODO: depending on DEVELOPMENT or DEPLOYMENT
-  leaveUrl: "https://www.zoomdemo.aankh.co/overlay", // use the proxied address in the server
+  leaveUrl: ZOOM_LEAVE_URL, // use the proxied address in the server
 
   // pass in the registrant's token if your meeting or webinar requires registration. More info here:
   // Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/build/meetings/join#join-registered
